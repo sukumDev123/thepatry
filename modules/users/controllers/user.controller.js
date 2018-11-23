008017c7717f5983271a34f98cd52223450ddb3d
sukumDev123 <kungo2009@hotmail.com>
2018-11-24 00:05:54 +0700

    create new user,find user by email. :airplane:
    I was control modules for create new user and find user and create token with firebase auth. :+1:

--- modules/users/controllers/user.controller.js
+++ modules/users/controllers/user.controller.js
@@ -3,9 +3,22 @@ import * as upre from "../presents/user.present"
 export const create_new_user = async (req, res, next) => {
   if (req.body) {
     try {
-      const userModel = new UserModel(...req.body)
+      const { email, password, name } = req.body
+      const userModel = new UserModel(email, password, name)
       const createUser = await upre.create_user(userModel)
       res.json(createUser)
+    } catch (error) {
+      next({ message: JSON.stringify(error), status: 500 })
+    }
+  }
+}
+export const loginWithEmail = async (req, res, next) => {
+  if (req.body) {
+    try {
+      const { email } = req.body
+      const findUserTouseUuid = await upre.findUser(email)
+      const createToken = await upre.getToken(findUserTouseUuid.uid)
+      res.json(createToken)
     } catch (error) {
       next({ message: JSON.stringify(error), status: 500 })
     }
--- modules/users/routes/user.route.js
+++ modules/users/routes/user.route.js
@@ -1,5 +1,6 @@
 import * as cont from "../controllers/user.controller"
 export const userRouter = router => {
   router.post("/user/createNewUser", cont.create_new_user)
+  router.post("/user/loginWithEmail", cont.loginWithEmail)
   return router
 }
